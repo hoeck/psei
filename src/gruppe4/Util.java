33c072bc9bac8f080b548c76d4009ebd5dd72017
hoeck <dfw407so@dd.bib.de>
2009-02-15 17:47:39 +0100

    added Util class

---
+++ src/gruppe4/Util.java
@@ -0,0 +1,19 @@
+package gruppe4;
+
+/**
+ * Utility functions.
+ *
+ */
+public class Util {
+
+    public static float max3f(float a, float b, float c)
+    {
+        return Math.max(Math.max(a,b),c);
+    }
+
+    public static float min3f(float m, float n, float o)
+    {
+        return Math.min(Math.min(m,n),o);
+    }
+
+}
